fe374234ac27aa6c4d28ef12948596fa47238fde
Matthew Meacham <mmeacham6@gmail.com>
2017-03-20 15:31:47 -0500

    Created php code for pulling data from db and creating the queue elements

--- assets/server/queue.php
+++ assets/server/queue.php
@@ -10,6 +10,51 @@
 			<img class='vita-header-curtain' src='../res/QueueCurtain.svg'>
 			<img class='vita-exit-fullscreen' src='../res/GenericExit.svg' onclick='alert("This ought to close the window")'>
 		</div>
+
+
+		<div class='vita-queue-wrapper'>
+			<div class='vita-queue-header'>
+				<div class='vita-queue-header-position'>#</div>
+				<div class='vita-queue-header-name'>Name</div>
+				<div class='vita-queue-header-wait'>Wait Time</div>
+			</div>
+			<div class='vita-queue'>
+				<?php
+					$servername = 'localhost';
+					$username = 'username';
+					$password = 'password';
+					$dbname = 'myDB';
+
+					$conn = new mysqli($servername, $username, $password, $dbname);
+					if ($conn->connect_error) {
+						die('Unable to connect to queue');
+					}
+
+		 			//TODO make this handle multiple locations.
+					$stmt = $conn->prepare('SELECT id, time, firstName, lastName FROM appointment
+						where (date = getdate() && archived = 0)');
+					$stmt->execute();
+					$results = $stmt->fetchAll();
+
+					for ($i = 0; $i < $results.length; $i++) {
+						$appointment = $results[i];
+						$currentDateTime = new DateTime();
+						//TODO the below might get stored differently in database, might need to rework
+						$appointmentDateTime = DateTime.createFromFormat('Y-m-d H:i:s', $appointment.date . ' ' . $appointment.time);
+						$timeDifference = $currentDateTime.diff($appointmentDateTime);
+						$waitTime = $timeDifference.i + ($timeDifference.h * 60);
+
+						echo "<div class='vita-queue-entry'>
+								<div class='vita-queue-entry-position'>" . ($i + 1) . "</div>
+								<div class='vita-queue-entry-name'>" . $appointment.firstName . " " . substr($appointment.lastName, 0, 1) . ".</div>
+								<div class='vita-queue-entry-wait'>" . $waitTime . " Minutes</div>
+							</div>";
+					}
+				?>
+			</div>
+		</div>
+
+
 		<div class='vita-queue-wrapper'>
 			<div class='vita-queue-header'>
 				<div class='vita-queue-header-position'>#</div>
