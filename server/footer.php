d78e6213dbc3459eda7a18b4a1ad339d1b908e72
Austin Schmidt <aus.tina.schmidt11@gmail.com>
2017-04-02 16:04:53 -0500

    less whitespace -final

--- server/footer.php
+++ server/footer.php
@@ -1,4 +1,3 @@
 <footer>
 	<script src='/assets/js/jquery-3.1.1.min.js'></script>
-
 </footer>
--- server/header.php
+++ server/header.php
@@ -12,5 +12,4 @@
 
     <link rel="stylesheet" href="/assets/css/normalize.css">
     <link rel="stylesheet" href="/assets/css/main.css">
-    
 </head>
