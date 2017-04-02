aa98ef588189d9c024d8ccb39a5098ff2d0abcd5
Matthew Meacham <mmeacham6@gmail.com>
2017-04-02 15:42:48 -0500

    Moved php code from server to the /queue/queue.php file

--- queue/index.php
+++ queue/index.php
@@ -46,9 +46,37 @@
         <script src="../assets/js/plugins.js"></script>
         <script src="../assets/js/main.js"></script>
 
-    	<script src='../assets/js/queue.js'></script>
+        <?php
+        	$server_name = 'localhost';
+        	$username = 'username';
+        	$password = 'password';
+        	$db_name = 'myDB';
 
-    	<?php require '../assets/server/queue.php' ?>
+        	$conn = new mysqli($server_name, $username, $password, $db_name);
+        	if ($conn->connect_error) {
+        		die('Unable to connect to queue');
+        	}
+
+        	//TODO make this handle multiple locations if necessary
+        	$stmt = $conn->prepare('SELECT id, time, firstName, lastName FROM appointment
+        		where (date = getdate() && archived = 0)');
+        	$stmt->execute();
+        	$results = $stmt->fetchAll();
+
+        	// We must only display the first letter of the last name, we do this server-side since we can't disclose the data client-side
+        	$appointments = [];
+        	foreach($results as $result) {
+        		$result['lastName'] = substr($result['lastName'], 0, 1);
+        		$appointments[] = $result;
+        	}
+
+        	echo json_encode($appointments);
+
+        	$stmt->close();
+        	$conn->close();
+        ?>
+
+        <script src='../assets/js/queue.js'></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
