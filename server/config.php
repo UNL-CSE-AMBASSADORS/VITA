0550f09c968f59736a037901823b831bc6a76847
Austin Schmidt <aus.tina.schmidt11@gmail.com>
2017-04-02 15:53:36 -0500

    header and footer

---
+++ server/config.php
@@ -0,0 +1,12 @@
+<?
+
+try {
+	require_once 'Login.class.php';
+
+	$db_user = 'root';
+	$db_pass = 'root';
+
+	$DB_CONN = new PDO('mysql:host=127.0.0.1;dbname=vita;', $db_user, $db_pass);
+} catch (Exception $e) {
+	print_r($e);
+}
---
+++ server/footer.php
@@ -0,0 +1,4 @@
+<footer>
+	<script src='/assets/js/jquery-3.1.1.min.js'></script>
+	<script src='/assets/js/queue.js'></script>
+</footer>
