0651ced90f54792e6089c346f18ba13e2d82d847
Austin Schmidt <aus.tina.schmidt11@gmail.com>
2017-04-30 15:36:35 -0500

    moved create user to top, slight style fixes

--- mysql scripts/loginDB.sql
+++ mysql scripts/loginDB.sql
@@ -5,32 +5,8 @@ DROP TABLE IF EXISTS PasswordReset;
 DROP TABLE IF EXISTS LoginHistory;
 DROP TABLE IF EXISTS User;
 
-CREATE TABLE `Login` (
-		`userId` INT UNSIGNED NOT NULL,
-	`failedLoginCount` INT UNSIGNED ZEROFILL NOT NULL DEFAULT 0,
-	`password` VARCHAR(255) NOT NULL,
-	`lockoutTime` TIMESTAMP NOT NULL DEFAULT 0,
-	foreign key (userId) references User(userId)
-	);
-
-CREATE TABLE `LoginHistory` (
-		`userId` INT UNSIGNED NOT NULL,
-	`timestamp` TIMESTAMP NOT NULL DEFAUloginloginLT CURRENT_TIMESTAMP,
-	`ipAddress` VARCHAR(95) NOT NULL,
-	foreign key (userId) references User(userId)
-	);
-	
-CREATE TABLE `PasswordReset` (
-		`userId` INT UNSIGNED NOT NULL,
-	`token` VARCHAR(255) NOT NULL,
-	`ipAddress` VARCHAR(95) NOT NULL,
-	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`archived` TINYINT NOT NULL DEFAULT 0,
-	foreign key (userId) references User(userId)
-	);
-
 CREATE TABLE `User` (
-		`userId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`userId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(355) NOT NULL,
 	`firstName` VARCHAR(65) NULL,
 	`lastName` VARCHAR(65) NULL,
@@ -39,16 +15,40 @@ CREATE TABLE `User` (
 	UNIQUE INDEX `userId_UNIQUE` (`userId` ASC)
 	);
 	
-INSERT INTO `vita`.`user`
+CREATE TABLE `Login` (
+	`userId` INT UNSIGNED NOT NULL,
+	`failedLoginCount` INT UNSIGNED ZEROFILL NOT NULL DEFAULT 0,
+	`password` VARCHAR(255) NOT NULL,
+	`lockoutTime` TIMESTAMP NOT NULL DEFAULT 0,
+	foreign key (userId) references User(userId)
+	);
+
+CREATE TABLE `LoginHistory` (
+	`userId` INT UNSIGNED NOT NULL,
+	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`ipAddress` VARCHAR(95) NOT NULL,
+	foreign key (userId) references User(userId)
+	);
+	
+CREATE TABLE `PasswordReset` (
+	`userId` INT UNSIGNED NOT NULL,
+	`token` VARCHAR(255) NOT NULL,
+	`ipAddress` VARCHAR(95) NOT NULL,
+	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`archived` TINYINT NOT NULL DEFAULT 0,
+	foreign key (userId) references User(userId)
+	);
+
+INSERT INTO `User`
 		(
-		`email`,
-		`firstName`,
-		`lastName`
+			`email`,
+			`firstName`,
+			`lastName`
 		)
 	VALUES
 		(
-		'test@gmail.com',
-		'John',
-		'Smith'
+			'test@gmail.com',
+			'John',
+			'Smith'
 		);
 
