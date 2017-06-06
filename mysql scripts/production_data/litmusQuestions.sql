1b0bfe071b17b30071c305655aafb54140f09664
Matthew Meacham <mmeacham6@gmail.com>
2017-06-05 20:53:00 -0500

    Added the follow up questions for self-employed

--- mysql scripts/production_data/litmusQuestions.sql
+++ mysql scripts/production_data/litmusQuestions.sql
@@ -37,18 +37,20 @@ INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId)
 
 -- Question 4
 INSERT INTO LitmusQuestion (string, orderIndex, tag)
-	VALUES ("Will your return have casulty losses?", 4, "casulty_losses");
+	VALUES ("If you are self-employed or own a home-based business, does it A) have a net loss, B) have more than $10,000 in expenses, C) have self-employed, SEP, SIMPLE, or qualified retirement plans, or D) have employees?", 4, "self_employed_followups");
 SET @lastInsertId = LAST_INSERT_ID();
 
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
-	VALUES ("No", 1, @lastInsertId);
+	VALUES ("Does Not Apply", 1, @lastInsertId);
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
-	VALUES ("Yes", 2, @lastInsertId);
+	VALUES ("No", 2, @lastInsertId);
+INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
+	VALUES ("Yes", 3, @lastInsertId);
 -- End Question 4
 
 -- Question 5
 INSERT INTO LitmusQuestion (string, orderIndex, tag)
-	VALUES ("Will your return have theft losses?", 5, "theft_losses");
+	VALUES ("Will your return have casulty losses?", 5, "casulty_losses");
 SET @lastInsertId = LAST_INSERT_ID();
 
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
@@ -59,7 +61,7 @@ INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId)
 
 -- Question 6
 INSERT INTO LitmusQuestion (string, orderIndex, tag)
-	VALUES ("Will you require a Schedule E (rental income)?", 6, "schedule_e");
+	VALUES ("Will your return have theft losses?", 6, "theft_losses");
 SET @lastInsertId = LAST_INSERT_ID();
 
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
@@ -69,8 +71,8 @@ INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId)
 -- End Question 6
 
 -- Question 7
-INSERT INTO LitmusQuestion (string, orderIndex, tag) 
-	VALUES ("Will you require a Schedule K-1 (partnership or trust income)?", 7, "schedule_k-1");
+INSERT INTO LitmusQuestion (string, orderIndex, tag)
+	VALUES ("Will you require a Schedule E (rental income)?", 7, "schedule_e");
 SET @lastInsertId = LAST_INSERT_ID();
 
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
@@ -80,8 +82,8 @@ INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId)
 -- End Question 7
 
 -- Question 8
-INSERT INTO LitmusQuestion (string, orderIndex, tag)
-	VALUES ("Do you have income from dividends, capital gains, or minimal brokerage transactions?", 8, "dividends_income");
+INSERT INTO LitmusQuestion (string, orderIndex, tag) 
+	VALUES ("Will you require a Schedule K-1 (partnership or trust income)?", 8, "schedule_k-1");
 SET @lastInsertId = LAST_INSERT_ID();
 
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
@@ -89,16 +91,21 @@ INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId)
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
 	VALUES ("Yes", 2, @lastInsertId);
 -- End Question 8
-    
+
 -- Question 9
 INSERT INTO LitmusQuestion (string, orderIndex, tag)
-	VALUES ("Will your return involve a current bankruptcy?", 9, "current_bankruptcy");
+	VALUES ("Do you have income from dividends, capital gains, or minimal brokerage transactions?", 9, "dividends_income");
+SET @lastInsertId = LAST_INSERT_ID();
 
+INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
+	VALUES ("No", 1, @lastInsertId);
+INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
+	VALUES ("Yes", 2, @lastInsertId);
 -- End Question 9
-
+    
 -- Question 10
 INSERT INTO LitmusQuestion (string, orderIndex, tag)
-	VALUES ("Will your return involve income from more than one state?", 10, "multiple_states");
+	VALUES ("Will your return involve a current bankruptcy?", 10, "current_bankruptcy");
 SET @lastInsertId = LAST_INSERT_ID();
 
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
@@ -106,3 +113,14 @@ INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId)
 INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
 	VALUES ("Yes", 2, @lastInsertId);
 -- End Question 10
+
+-- Question 11
+INSERT INTO LitmusQuestion (string, orderIndex, tag)
+	VALUES ("Will your return involve income from more than one state?", 11, "multiple_states");
+SET @lastInsertId = LAST_INSERT_ID();
+
+INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
+	VALUES ("No", 1, @lastInsertId);
+INSERT INTO PossibleAnswer (string, orderIndex, litmusQuestionId) 
+	VALUES ("Yes", 2, @lastInsertId);
+-- End Question 11
