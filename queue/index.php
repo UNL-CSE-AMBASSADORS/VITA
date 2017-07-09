dd3e13625bc53c4d9eade789310e7911f732d7d1
JakeKo <wijakoperski@gmail.com>
2017-07-09 14:05:43 -0500

    Removed unnecessary svgs and began queue nav-bar and dashboard

--- queue/index.php
+++ queue/index.php
@@ -9,13 +9,77 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-		<div class='vita-header'>
-			<div class='vita-volunteer-count'>Volunteers on duty: 0</div>
-			<img class='vita-header-curtain' src='../assets/res/queue_curtain.svg'>
-			<img class='vita-exit-fullscreen' src='../assets/res/generic_exit.svg'>
+		<div class='section nav-bar'>
+			<div class='wrpr-left flex menu-icon-wrpr'>
+				<img class='menu-icon' src='../assets/res/menu_white.svg'>
+			</div>
+			<div class='wrpr-left flex page-lbl-wrpr'>
+				<div class='page-lbl'>Queue</div>
+			</div>
+			<div class='wrpr-right flex site-lbl-wrpr'>
+				<div class='site-lbl'>Center for People in Need</div>
+			</div>
 		</div>
 
-		<div class='vita-queue-wrapper'>
+		<div class='section dashboard'>
+			<div class='wrpr-left flex volunteers-wrpr'>
+				<div class='flex volunteers'>
+					<div class='volunteers-lbl'>Volunteers:</div>
+					<div class='volunteers-count'>10</div>
+				</div>
+			</div>
+			<div class='wrpr-left flex queue-size-wrpr'>
+				<div class='flex queue-size'>
+					<div class='queue-size-lbl'>In Queue:</div>
+					<div class='queue-size-count'>23</div>
+				</div>
+			</div>
+			<div class='wrpr-right flex clock-wrpr'>
+				<div class='flex clock'>
+					<div class='clock-time'>08:45</div>
+					<div class='clock-period'>
+						<div class='clock-am inactive-period'>AM</div>
+						<div class='clock-pm'>PM</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class='section queue'>
+			<div class='queue-container'>
+				<div class='queue-header-container'>
+					<div class='queue-header'>
+						<div class='wrpr-left queue-position-wrpr'>
+							<div class='queue-lbl'>Position</div>
+						</div>
+						<div class='wrpr-left queue-name-wrpr'>
+							<div class='queue-lbl'>Name</div>
+						</div>
+						<div class='wrpr-right queue-time-wrpr'>
+							<div class='queue-lbl'>Time</div>
+						</div>
+					</div>
+				</div>
+
+				<div class='queue-table-container'>
+					<div class='queue-table'>
+						<div class='queue-table-record'>
+							<div class='wrpr-left queue-position-wrpr'>
+								<div class='queue-field'>1</div>
+							</div>
+							<div class='wrpr-left queue-name-wrpr'>
+								<div class='queue-field'>Matt M.</div>
+							</div>
+							<div class='wrpr-right queue-time-wrpr'>
+								<div class='queue-field'>8:30 AM</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class='vita-queue-wrpr'>
 			<div class='vita-queue-header'>
 				<div class='vita-queue-header-position'>#</div>
 				<div class='vita-queue-header-name'>Name</div>
