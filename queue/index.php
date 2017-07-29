a36d4bcdc4a4aae143fa3341dd0b1cf1e1020674
JakeKo <wijakoperski@gmail.com>
2017-07-28 23:53:40 -0500

    Miscellaneous style cleaning

--- queue/index.php
+++ queue/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js" lang="">
+<html class='no-js theme-light' lang="">
     <head>
 		<title>Queue Test</title>
 		<?php require_once '../server/header.php' ?>
@@ -11,7 +11,7 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->
 		<?php require_once '../components/nav.php' ?>
-		<div class='section theme-light dashboard'>
+		<div class='section dashboard'>
 			<div class='sub-section-full'>
 				<div class='wrap-left flex volunteers-wrap'>
 					<div class='volunteers-lbl'>Volunteers:</div>
@@ -36,13 +36,13 @@
 			</div>
 		</div>
 
-		<div class='section queue'>
+		<div class='section theme-light queue'>
 			<div class='sub-section flex queue-header'>
 				<div class='wrap-left queue-position-wrap'>Pos.</div>
 				<div class='wrap-left queue-name-wrap'>Name</div>
 				<div class='wrap-right queue-time-wrap'>Time</div>
 			</div>
-			<div class='sub-section flex queue-table'></div> 
+			<div class='sub-section theme-white flex queue-table'></div> 
 		</div> 
 		<?php require_once '../server/footer.php' ?>
 	</body>
