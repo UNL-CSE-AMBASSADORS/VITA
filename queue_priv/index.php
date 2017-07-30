7cf5ebe3e288010ac953c046234585d6089808e4
JakeKo <wijakoperski@gmail.com>
2017-07-30 15:54:05 -0500

    Private queue wireframing

--- queue_priv/index.php
+++ queue_priv/index.php
@@ -3,7 +3,7 @@
     <head>
 		<title>Queue Test</title>
 		<?php require_once '../server/header.php' ?>
-		<link rel='stylesheet' href='../assets/css/queue.css'>
+		<link rel='stylesheet' href='../assets/css/queue_priv.css'>
 		<meta http-equiv='refresh' content='600'/>
     </head>
     <body>
@@ -11,39 +11,27 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->
 		<?php require_once '../components/nav.php' ?>
-		<div class='section dashboard'>
-			<div class='sub-section-full'>
-				<!-- <div class='wrap-left flex volunteers-wrap'>
-					<div class='volunteers-lbl'>Volunteers:</div>
-					<div class='volunteers-count'></div>
-				</div> -->
-				<div class='wrap-left queue-size-wrap'>
-					<div class='queue-size-lbl'>Queue:</div>
-					<div class='queue-size-count'></div>
+		<div class='section box theme-light queue-details'>
+			<div class='theme-white flex queue'>
+				<div class='sub-section flex queue-header'>
+					<div class='wrap-left queue-position-wrap'>Pos.</div>
+					<div class='wrap-left queue-name-wrap'>Name</div>
+					<div class='wrap-right queue-time-wrap'>Time</div>
 				</div>
-				<div class='wrap-right clock-wrap'>
-					<div class='clock-time'></div>
-					<div class='clock-period'>
-						<div class='clock-am'>AM</div>
-						<div class='clock-pm'>PM</div>
-					</div>
-				</div>
-				<div class='wrap-right flex date-wrap'>
-					<i class='material-icons no-select date-back'>keyboard_arrow_up</i>
-					<div class='date'></div>
-					<i class='material-icons no-select date-forward'>keyboard_arrow_down</i>
-				</div>
+				<div class='sub-section theme-white flex queue-table'></div> 
+			</div>
+			<div class='theme-white flex details'>
+
 			</div>
 		</div>
-
-		<div class='section theme-light queue'>
-			<div class='sub-section flex queue-header'>
+		<!-- <div class='section theme-light queue'>
+			 <div class='sub-section flex queue-header'>
 				<div class='wrap-left queue-position-wrap'>Pos.</div>
 				<div class='wrap-left queue-name-wrap'>Name</div>
 				<div class='wrap-right queue-time-wrap'>Time</div>
-			</div>
+			</div> 
 			<div class='sub-section theme-white flex queue-table'></div> 
-		</div> 
+		</div>  -->
 		<?php require_once '../server/footer.php' ?>
 	</body>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js'></script>
