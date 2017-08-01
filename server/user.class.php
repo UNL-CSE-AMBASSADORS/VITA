a03944abad78fc558c4fde8421c976dbce48eb67
Austin Schmidt <aus.tina.schmidt11@gmail.com>
2017-07-31 21:50:07 -0500

    Small class that enables easy checking of user things.

---
+++ server/user.class.php
@@ -0,0 +1,82 @@
+<?
+require_once 'login.class.php';
+require_once 'callbacks.php';
+require_once 'config.php';
+
+// META - Can give users permissions(inclusive?!)
+define('PrivilegeControl', 'PrivilegeControl');
+
+// Can Unassign users from a shift
+define('UserShiftControl', 'UserShiftControl');
+
+// Can Modify Appointments
+define('AppointmentControl', 'AppointmentControl');
+
+
+/**
+* Class for easily checking user permissions
+* Simply instantiate and use
+*/
+
+class User
+{
+
+	/**
+	* Returns user id, will return FALSE if not logged in!
+	* 
+	* @return int
+	*/ 
+	public function getUserId(){
+		$LOGIN = getLoginClass();
+
+		## Check Login Status
+		if(!$LOGIN->checkLogin()){
+			$LOGIN->logout();
+			return false;
+		}
+
+		return $_SESSION['USER__ID'];
+	}
+
+
+	/**
+	* Checks whether the user is still lagged in
+	* 
+	* @return boolean
+	*/ 
+	public function isLoggedIn(){
+		$LOGIN = getLoginClass();
+
+		return $LOGIN->checkLogin();
+	}
+
+	/**
+	* Checks whether the user has specifed permission
+	* 
+	* @return boolean
+	*/ 
+	public function hasPermission($permissionTag){
+		GLOBAL $DB_CONN;
+
+		$userId = $this->getUserId();
+
+		$query = "SELECT userId 
+			FROM userPrivilege
+				INNER JOIN privilege ON privilege.privilegeId = userprivilege.userPrivilegeId
+			WHERE 1=1
+				AND userId = ?
+				AND tag = ?";
+
+
+		$stmt = $DB_CONN->prepare($query);
+		$stmt->execute(array($userId,$permissionTag));
+
+		$results = $stmt->fetchAll();
+
+		if(empty($results)){
+			return false;
+		}else{
+			return true;
+		}
+	}
+}
