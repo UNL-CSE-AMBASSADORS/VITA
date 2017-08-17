d812c9cff5ff4ff8b0edf474f7c4d9cadd0d8116
JakeKo <wijakoperski@gmail.com>
2017-08-17 17:33:08 -0500

    Added cancel functionality to the private queue

--- assets/js/queue_priv.js
+++ assets/js/queue_priv.js
@@ -1,29 +1,42 @@
 var REFRESH_SEC = 15,
 	displayDate = new Date(),
 	monthStrings = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
-	refreshing = null;
+	refreshing = null,
+	openedAppointmentId = 0;
 
 $('.date-wrap').on('click', '.date-back', hideDetails);
 $('.date-wrap').on('click', '.date-forward', hideDetails);
-$('.details-controls').on('click', '.details-close', hideDetails);
 $('.details-controls').on('click', '.details-close', function() {
 	$('.queue-record').removeClass('theme-light');
+	hideDetails()
 });
 
+$('.details-controls').on('click', '.details-cancel', function() {
+	$.get({
+		data: `id=${openedAppointmentId}&action=cancel`,
+		url: '/server/queue_priv.php',
+		success: function(r) {
+			hideDetails();
+			populateQueue();
+		}
+	});
+});
+
+// Retrieves and displays the data associated with the selected appointment
 $('.queue-table').on('click', '.queue-record', function() {
+	openedAppointmentId = $(this).find('.queue-record-id').html();
+
 	// Adds a soft highlight to the last selected appointment
 	$('.queue-record').removeClass('theme-light');
 	$(this).addClass('theme-light');
 
 	$.get({
-		data: `id=${$(this).find('.queue-record-id').html()}&action=display`,
+		data: `id=${openedAppointmentId}&action=display`,
 		url: '/server/queue_priv.php',
 		dataType: 'json',
 		cache: false,
 		success: function(r) {
-			var time = new Date(r[0].scheduledTime),
-				h = time.getHours() % 12,
-				m = time.getMinutes();
+			var time = new Date(r[0].scheduledTime), h = time.getHours() % 12, m = time.getMinutes();
 			if (h === 0) h = 12;
 			if (m < 10) m = `0${m}`;
 
@@ -49,4 +62,5 @@ function refresh() {
 
 function hideDetails() {
 	$('.details').css('display', 'none');
+	openedAppointmentId = 0;
 }
--- server/queue_priv.php
+++ server/queue_priv.php
@@ -4,24 +4,32 @@
 
 	switch($_GET['action']) {
 		case 'display': displayAppointment($_GET['id']); break;
+		case 'cancel': cancelAppointment($_GET['id']); break;
 		default: break;
 	}
 
-
-
-
-
 	function displayAppointment($id) {
-		$stmt = $GLOBALS['DB_CONN']->prepare(
+		$stmt = $GLOBALS['conn']->prepare(
 			"SELECT Appointment.scheduledTime, Client.firstName, Client.lastName, Client.emailAddress, Site.title
 			FROM Appointment
 			JOIN Client ON Appointment.clientId = Client.clientId
 			JOIN Site ON Appointment.siteId = Site.siteId
-			WHERE Appointment.AppointmentId = ?"
+			WHERE Appointment.appointmentId = ?"
 		);
 	
 		$stmt->execute(array($id));
 		$appointment = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		echo json_encode($appointment);
+		$stmt = null;
+	}
+
+	function cancelAppointment($id) {
+		$stmt = $GLOBALS['conn']->prepare(
+			"UPDATE Appointment
+			SET Appointment.archived = TRUE
+			WHERE Appointment.appointmentId = ?"
+		);
+	
+		$stmt->execute(array($id));
 		$stmt = null;
 	}
