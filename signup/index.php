41f9484cc13b884951384d13e023282608cd8313
Spencer Collins <collinsspencer97@gmail.com>
2017-09-02 01:26:25 -0500

    Convert signup page to Bootstrap

--- signup/index.php
+++ signup/index.php
@@ -5,7 +5,7 @@
 	<title>VITA Appointment Signup</title>
 	<?php require_once "$root/server/header.php" ?>
 	<link rel="stylesheet" href="/assets/css/form.css">
-	<link rel="stylesheet" href="/assets/css/signup.css">
+	<link rel="stylesheet" href="/signup/signup.css">
 </head>
 <body>
 	<?php
@@ -13,45 +13,47 @@
 		require_once "$root/components/nav.php";
 	?>
 
-	<div class="section">
-		<div class="sub-section">
-      <div id="responsePlaceholder" style="display: none;"></div>
-			<form class="cmxform" id="vitaSignupForm" method="post" action="" autocomplete="off">
-				<h2 class="form-title">Sign Up for a VITA Appointment</h2>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col col-12 col-sm-8">
+				<div id="responsePlaceholder" style="display: none;"></div>
+				<form class="cmxform" id="vitaSignupForm" method="post" action="" autocomplete="off">
+					<h2 class="form-title">Sign Up for a VITA Appointment</h2>
 
-				<div class="form-textfield">
-					<input type="text" name="firstName" id="firstName">
-					<span class="form-bar"></span>
-					<label class="form-label form-required" for="firstName">First Name</label>
-				</div>
+					<div class="form-textfield">
+						<input type="text" name="firstName" id="firstName">
+						<span class="form-bar"></span>
+						<label class="form-label form-required" for="firstName">First Name</label>
+					</div>
 
-				<div class="form-textfield">
-					<input type="text" name="lastName" id="lastName" required>
-					<span class="form-bar"></span>
-					<label class="form-label form-required" for="lastName">Last Name</label>
-				</div>
+					<div class="form-textfield">
+						<input type="text" name="lastName" id="lastName" required>
+						<span class="form-bar"></span>
+						<label class="form-label form-required" for="lastName">Last Name</label>
+					</div>
 
-				<div class="form-textfield">
-					<input type="email" name="email" id="email" required>
-					<span class="form-bar"></span>
-					<label class="form-label form-required" for="email">Email</label>
-				</div>
+					<div class="form-textfield">
+						<input type="email" name="email" id="email" required>
+						<span class="form-bar"></span>
+						<label class="form-label form-required" for="email">Email</label>
+					</div>
 
-				<div class="form-textfield">
-					<input type="text" name="phone" id="phone" required>
-					<span class="form-bar"></span>
-					<label class="form-label form-required" for="phone">Phone Number</label>
-				</div>
+					<div class="form-textfield">
+						<input type="text" name="phone" id="phone" required>
+						<span class="form-bar"></span>
+						<label class="form-label form-required" for="phone">Phone Number</label>
+					</div>
 
-				<h3 class="form-subheading">Appointment Information</h3>
+					<h3 class="form-subheading">Appointment Information</h3>
 
-				<?php
-					require_once "$root/server/signup.php";
-					getLitmusQuestions();
-				?>
+					<?php
+						require_once "$root/server/signup.php";
+						getLitmusQuestions();
+					?>
 
-				<input type="submit" value="Submit" class="submit button vita-background-primary">
-			</form>
+					<input type="submit" value="Submit" class="submit button vita-background-primary">
+				</form>
+			</div>
 		</div>
 	</div>
 	<?php require_once "$root/server/footer.php" ?>
