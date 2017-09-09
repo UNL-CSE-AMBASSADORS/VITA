037275c931195af284025c8db56074d83e5bf412
Spencer Collins <collinsspencer97@gmail.com>
2017-09-09 12:42:49 -0500

    Update private queue

--- queue/private.php
+++ queue/private.php
@@ -21,8 +21,8 @@
 
 	<!-- Header section -->
 	<div class="container-fluid dashboard bg-light py-3" ng-cloak>
-		<div class="d-flex flex-column flex-sm-row justify-content-sm-between justify-content-center align-items-center">
-			<div class="d-flex flex-row pb-3 pb-sm-0">
+		<div class="d-flex flex-row justify-content-between align-items-center">
+			<div class="d-flex flex-row">
 				<div class="queue-size-lbl">Queue:</div>
 				<div class="queue-size-count">{{appointments.length}}</div>
 			</div>
@@ -43,44 +43,84 @@
 		</div>
 	</div>
 
-	<!-- Body Section with list of clients -->
-	<div class="container-fluid queue" ng-if="appointments.length > 0" ng-cloak>
-		<div class="row queue-header py-2 bg-secondary text-light font-weight-bold">
-			<div class="col col-1 queue-id">Id.</div>
-			<div class="col col-8 queue-name">Name</div>
-			<div class="col col-3 queue-time">Time</div>
-		</div>
-		<div class="row queue-row" ng-repeat="appointment in appointments | orderBy:'scheduledTime'" ng-class-even="'bg-light'">
-			<div class="col col-1 queue-id">{{appointment.appointmentId}}</div>
-			<div class="col col-8 queue-name">{{appointment.firstName}} {{appointment.lastName}}.</div>
-			<div class="col col-3 queue-time">{{appointment.scheduledTime | date: "h:mm a"}}</div>
-		</div>
-	</div>
-	<div class="container-fluid queue" ng-if="appointments.length == 0" ng-cloak>
-		<div class="row d-flex justify-content-center">
-			<div class="my-5">
-				There are no appointments on this day.
+	<!-- Body Section -->
+	<div class="relative-wrapper clearfix">
+		<div class="fill-remaining d-flex flex-column flex-md-row">
+
+			<!-- Queue Section -->
+			<div class="queue-scroll-section container-fluid" ng-cloak>
+				<!-- Search box -->
+				<div class="queue-search py-3">
+					<input class="w-100" type="text" ng-model="clientSearch" placeholder="Search for a client by name or appointment ID" />
+				</div>
+				<!-- List of clients -->
+				<div class="queue" ng-if="appointments.length > 0" ng-cloak>
+					<div class="row queue-row py-1 pointer"
+							 ng-repeat="appointment in appointments | orderBy:'scheduledTime' | searchFor: clientSearch"
+							 ng-class-odd="'bg-light'"
+							 ng-click="selectClient(appointment)">
+						<div class="col">
+							<div class="d-flex flex-column">
+								<div class="d-flex flex-nowrap justify-content-between">
+									<div class="queue-name font-weight-bold">{{appointment.firstName}} {{appointment.lastName}}.</div>
+									<div class="queue-time">{{appointment.scheduledTime | date: "h:mm a"}}</div>
+								</div>
+								<div class="d-flex flex-nowrap justify-content-between">
+									<div class="queue-id">#{{appointment.appointmentId}}</div>
+									<div class="queue-status">
+										<span class="badge badge-pill badge-primary">Checked-in</span>
+										<span class="badge badge-pill badge-primary">Task 2</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<p ng-show="(appointments | searchFor: clientSearch).length == 0">No results for "{{clientSearch}}"</p>
+					<!-- <p ng-hide="filteredBars.length">Nothing here!</p> -->
+				</div>
+				<!-- Default message if there are no appointments on the selected date -->
+				<div class="queue" ng-if="appointments.length == 0" ng-cloak>
+					<div class="row d-flex justify-content-center">
+						<div class="my-5">
+							There are no appointments on this day.
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Client/Appointment Info Section -->
+			<div class="client-info-section container-fluid d-flex py-3">
+				<!-- Currently selected client -->
+				<div class="client d-flex align-items-start flex-column w-100" ng-if="client != null" ng-cloak>
+						<div class="client-name">{{client.firstName}} {{client.lastName}}.</div>
+						<div class="client-time">Scheduled Appointment Time: {{client.scheduledTime | date: "h:mm a"}}</div>
+
+						<!-- The following are a couple of options for client progress/"workflow" -->
+						<div class="progress w-100 my-2">
+							<div class="progress-bar" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+							<div class="progress-bar bg-success" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+							<div class="progress-bar bg-info" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+						</div>
+
+						<div class="client-progress d-flex flex-column">
+							<span class="my-1 badge badge-pill badge-primary">Check-in</span>
+							<span class="my-1 badge badge-pill badge-primary">Task 2</span>
+							<span class="my-1 badge badge-pill badge-secondary">Complete Paperwork</span>
+							<span class="my-1 badge badge-pill badge-secondary">Check-out</span>
+						</div>
+
+						<div class="client-appointmentId mt-auto">Appointment ID: {{client.appointmentId}}</div>
+				</div>
+				<!-- Default message if no appointment is selected -->
+				<div class="client d-flex justify-content-center w-100" ng-if="client == null" ng-cloak>
+					<div class="my-5">
+						Select a client
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 
 	<?php require_once "../server/footer.php" ?>
 </body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min.js"></script>
-<script src="/queue/queue_common.js"></script>
-<script src="/queue/queue_private.js"></script>
-<script class="queue-record-template" type="text/template">
-	<div class="queue-record" data-appointment-id="{{id}}">
-		<div class="wrap-left queue-position-wrap">{{position}}</div>
-		<div class="wrap-left queue-name-wrap">{{name}}</div>
-		<div class="wrap-right queue-time-wrap">
-			{{#isOnTime}}<div class="flex queue-tag on-time-tag">OT</div>{{/isOnTime}}
-			{{^isOnTime}}
-				{{#isPresent}}<div class="flex queue-tag late-tag">LT</div>{{/isPresent}}
-				{{^isPresent}}<div class="flex queue-tag no-show-tag">NS</div>{{/isPresent}}
-			{{/isOnTime}}
-			{{time}}
-		</div>
-	</div>
-</script>
 </html>
--- queue/queue_private.js
+++ queue/queue_private.js
@@ -1,4 +1,47 @@
 queueApp.controller("QueuePrivateController", function($scope, $controller, QueueService) {
 	$scope.test = "world";
 	angular.extend(this, $controller('QueueController', {$scope: $scope}));
+
+	// $scope.client = {
+	// 	appointmentId: 6,
+	// 	scheduledTime: '2017-09-09T10:10:39.000Z',
+	// 	firstName: 'Bob',
+	// 	lastName: 'Smith',
+	// };
+
+	$scope.selectClient = function(client) {
+		$scope.client = client;
+	};
+});
+
+queueApp.filter('searchFor', function(){
+
+	// All filters must return a function. The first parameter
+	// is the data that is to be filtered, and the second is an
+	// argument that may be passed with a colon (searchFor:searchString)
+
+	return function(arr, searchString){
+
+		if(!searchString){
+			return arr;
+		}
+
+		var result = [];
+
+		searchString = searchString.toLowerCase();
+
+		// Using the forEach helper method to loop through the array
+		angular.forEach(arr, function(item){
+
+			if(item.name.toLowerCase().indexOf(searchString) !== -1 ||
+				 item.appointmentId.toString().indexOf(searchString) !== -1 ||
+				 ("#" + item.appointmentId.toString()).indexOf(searchString) !== -1){
+				result.push(item);
+			}
+
+		});
+
+		return result;
+	};
+
 });
