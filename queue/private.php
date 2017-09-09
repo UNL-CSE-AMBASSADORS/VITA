07a137fa2c91a58a50ac31e51a37d0334394031f
Spencer Collins <collinsspencer97@gmail.com>
2017-09-09 12:45:03 -0500

    Move duplicated header content to separate file

--- queue/private.php
+++ queue/private.php
@@ -20,28 +20,9 @@
 	?>
 
 	<!-- Header section -->
-	<div class="container-fluid dashboard bg-light py-3" ng-cloak>
-		<div class="d-flex flex-row justify-content-between align-items-center">
-			<div class="d-flex flex-row">
-				<div class="queue-size-lbl">Queue:</div>
-				<div class="queue-size-count">{{appointments.length}}</div>
-			</div>
-			<md-datepicker
-				ng-model="currentDate"
-				ng-change="updateAppointmentInformation()"
-				md-placeholder="Enter date"
-				md-min-date="today"
-				md-hide-icons="calendar">
-			</md-datepicker>
-			<div class="d-none d-sm-flex flex-row align-items-center">
-				<div class="clock-time">{{updateTime | date: "h:mm"}}</div>
-				<div class="clock-period d-flex flex-column align-items-center ml-1">
-					<div class="clock-am" ng-class="isAm ? '':'inactive-period'">AM</div>
-					<div class="clock-pm" ng-class="isAm ? 'inactive-period':''">PM</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php
+		require_once "$root/queue/queue_header.php";
+	?>
 
 	<!-- Body Section -->
 	<div class="relative-wrapper clearfix">
