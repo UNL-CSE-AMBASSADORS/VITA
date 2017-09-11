7ec1d38a4de5af0b558f0b2cd5fe8fbd02c1bf99
Austin Schmidt <aus.tina.schmidt11@gmail.com>
2017-09-11 18:18:46 -0500

    Cleaner checking on callbacks, removed useless constants from user class

--- server/user.class.php
+++ server/user.class.php
@@ -3,9 +3,6 @@ require_once 'login.class.php';
 require_once 'callbacks.php';
 require_once 'config.php';
 
-define('PermissionControl', 'PermissionControl');
-define('UserShiftControl', 'UserShiftControl');
-define('AppointmentControl', 'AppointmentControl');
 
 
 /**
