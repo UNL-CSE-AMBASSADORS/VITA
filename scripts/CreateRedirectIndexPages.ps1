b7b09d68e9dbda66be0b7578ad24940857c0e55e
Matthew Meacham <mmeacham6@gmail.com>
2017-11-05 12:48:47 -0600

    Fix php code in here string

--- scripts/CreateRedirectIndexPages.ps1
+++ scripts/CreateRedirectIndexPages.ps1
@@ -6,9 +6,10 @@ $path = '../'
 foreach ($directory in Get-ChildItem -recurse -dir $path) {
 	if (!(Test-Path ($directory.FullName + '\index.php'))) {
 		New-Item ($directory.FullName + '\index.php') -type file -value @"
-			"<?php 
-			$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-			require_once "$root/server/redirect.php";
+<?php 
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once "$root/server/redirect.php";
+?>
 "@
 	} 
 } 
