c37afb5f78f9700c5845ba5c03105753bd53d71a
Matthew Meacham <mmeacham6@gmail.com>
2017-11-12 19:02:36 -0600

    Got webpack working with babel/uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './lib/management/appointments/appointments.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: {
+		  appointments: './management/appointments/appointments.js',
+		  login: './login/login.js',
+		  queue: './queue/queue.js',
+		  queue_private: './queue/queue_private.js',
+		  queue_service: './queue/queue_service.js',
+		  register: './register/register.js',
+		  signup: './signup/signup.js'
+	},
+	output: {
+		filename: '[name]_bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		
+	]
 };
+
+if(process.env.NODE_ENV === 'production'){
+	module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin())
+}
