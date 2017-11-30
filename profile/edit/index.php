3f2b15bdb8289bc2ad16352d9738faa5145b63a6
Rohan Thakker <rohanthakker@pcp117165pcs.unl.edu>
2017-11-30 01:13:21 -0600

    READDED LOGIN VERIFY

--- profile/edit/index.php
+++ profile/edit/index.php
@@ -1,12 +1,12 @@
 <?php
-	// $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-     //
-	// require_once "$root/server/user.class.php";
-	// $USER = new User();
-	// if (!$USER->isLoggedIn()) {
-	// 	header("Location: /unauthorized");
-	// 	die();
-	// }
+	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+
+	require_once "$root/server/user.class.php";
+	$USER = new User();
+	if (!$USER->isLoggedIn()) {
+		header("Location: /unauthorized");
+		die();
+	}
 ?>
 <!DOCTYPE html>
 <html>
