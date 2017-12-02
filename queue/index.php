ffd7ad4e05f6b7c64603659f000fb9e0d018ac0e
Matthew Meacham <mmeacham6@gmail.com>
2017-12-01 21:36:15 -0600

    Enable signed in users to reach the public queue

--- queue/index.php
+++ queue/index.php
@@ -4,7 +4,7 @@
 
 	require_once "$root/server/user.class.php";
 	$USER = new User();
-	if ($USER->isLoggedIn()) {
+	if ($USER->isLoggedIn() && !isset($_REQUEST['public'])) { // we enable logged in users to get to the public queue by having ?public in the URL
 		require_once "$root/queue/private.php";
 	} else {
 		require_once "$root/queue/public.php";
