ddd274f8ee4b6511e2bc0590564a359c74faa60d
JakeKo <wijakoperski@gmail.com>
2017-12-04 20:24:22 -0600

    Modified styling of unauthorized page to mimic UNL styling

--- unauthorized/index.php
+++ unauthorized/index.php
@@ -3,6 +3,8 @@
 <html class="no-js" lang="">
 <head>
 	<title>Unauthorized</title>
+	<link rel="stylesheet" href="/assets/css/global.css">
+	<link rel="stylesheet" href="/unauthorized/unauthorized.css">
 	<?php require_once "$root/server/header.php" ?>
 </head>
 <body>
@@ -12,8 +14,9 @@
 	?>
 
 	<div class="container">
-		<div class='h1 mt-5'>401 - Unauthorized</div>
-		<div class='h5'>You are not authorized to access this page. Contact your site coordinator if you feel this to be in error.</div>
+		<div class='mt-5 error-number'>401</div>
+		<div class='error-name'>Unauthorized</div>
+		<div class='mt-3 error-description'>You are not authorized to access this page.<br>Contact your site coordinator if you feel this to be an error.</div>
 	</div>
 
 	<?php require_once "$root/server/footer.php" ?>
