6e01f7cb6eafa42ef67b784798471b2ffa8d4be8
Matthew Meacham <mmeacham6@gmail.com>
2017-12-19 20:25:35 -0600

    Change page title

--- profile/index.php
+++ profile/index.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html class="no-js" lang="">
 <head>
-	<title>VITA Profile</title>
+	<title>Profile</title>
 	<?php require_once "$root/server/header.php" ?>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css" />
