95b62c171971ed72f573f883e1b9d81dbebc29eb
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2017-12-29 15:33:36 -0600

    Update login page to UNL Styling

--- login/login.js
+++ login/login.js
@@ -1,88 +1,90 @@
-$(function(){
-	console.log("%cSTOP! \n%cDo NOT paste/type anything here under any circumstance.", "color: red; font-size:36px;", "color: black; font-size: 12pt");
-	LoginControls();
-});
+require(['jquery'], function($) {
+	// login.php functions
+	var LoginControls = function(){
+		
+		// Focus Email Field
+		$("#login_email").focus();
 
-// login.php functions
-var LoginControls = function(){
-	
-	// Focus Email Field
-	$("#login_email").focus();
+		// Toogle Forms
+		$(".toggle_form").click(function(){
+			$("#login_form")[0].reset();
+			$("#register_form")[0].reset();
+			$("#register_success").hide();
+			$("#register_form, #register_info").show();
+			$("#login_panel,#register_panel").toggle("slow", function(){
+				if($("#login_form").is(':visible')){
+					$("#login_email").focus();
+				}else{
+					$("#register_email").focus();
+				}
+			});
 
-	// Toogle Forms
-	$(".toggle_form").click(function(){
-		$("#login_form")[0].reset();
-		$("#register_form")[0].reset();
-		$("#register_success").hide();
-		$("#register_form, #register_info").show();
-		$("#login_panel,#register_panel").toggle("slow", function(){
-			if($("#login_form").is(':visible')){
-				$("#login_email").focus();
+		});
+
+		$("#login_form").on('submit', function(e) {
+			// Prevent form submission
+			e.preventDefault();
+
+			// Define Fields
+			var email = $("#login_email").val();
+			var password = $("#login_password").val();
+
+			if(email && password){
+				// Ajax
+				$.ajax({
+					dataType: 'json',
+					method: 'POST',
+					url: '/server/callbacks.php',
+					data: {
+						callback: 'login',
+						email: email,
+						password: password
+					},
+					success: function(response){
+						if(response.success){
+							window.location = response.redirect;
+						}else{
+							alert(response.error);
+						}
+					}
+				});
 			}else{
-				$("#register_email").focus();
+
 			}
 		});
 
-	});
+		$("#register_form").on('submit', function(e) {
 
-	$("#login_form").on('submit', function(e) {
-		// Prevent form submission
-		e.preventDefault();
+			// Prevent form submission
+			e.preventDefault();
 
-		// Define Fields
-		var email = $("#login_email").val();
-		var password = $("#login_password").val();
+			// Define Fields
+			var email = $("#register_email").val();
 
-		if(email && password){
-			// Ajax
-			$.ajax({
-				dataType: 'json',
-				method: 'POST',
-				url: '/server/callbacks.php',
-				data: {
-					callback: 'login',
-					email: email,
-					password: password
-				},
-				success: function(response){
-					if(response.success){
-						window.location = response.redirect;
-					}else{
-						alert(response.error);
+			if(email){
+				$.ajax({
+					dataType: 'json',
+					method: 'POST',
+					url: '/server/callbacks.php',
+					data: {
+						callback: 'register',
+						email: email
+					},
+					success: function(response){
+						if(response.success){
+							$("#register_form, #register_info").hide();
+							$("#register_success").fadeIn();
+						}else{
+							alert(response.error);
+						}
 					}
-				}
-			});
-		}else{
+				});
+			}
+		});
+	};
 
-		}
+	$(function(){
+		console.log("%cSTOP! \n%cDo NOT paste/type anything here under any circumstance.", "color: red; font-size:36px;", "color: black; font-size: 12pt");
+		LoginControls();
 	});
-
-	$("#register_form").on('submit', function(e) {
-
-		// Prevent form submission
-		e.preventDefault();
-
-		// Define Fields
-		var email = $("#register_email").val();
-
-		if(email){
-			$.ajax({
-				dataType: 'json',
-				method: 'POST',
-				url: '/server/callbacks.php',
-				data: {
-					callback: 'register',
-					email: email
-				},
-				success: function(response){
-					if(response.success){
-						$("#register_form, #register_info").hide();
-						$("#register_success").fadeIn();
-					}else{
-						alert(response.error);
-					}
-				}
-			});
-		}
-	});
-};
+});
