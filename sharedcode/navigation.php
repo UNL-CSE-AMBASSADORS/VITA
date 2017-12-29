e2247ab52bc2f4fa080f08d1a9474da6970dfa11
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2017-12-29 16:23:57 -0600

    Fix some links in the navigation

--- sharedcode/navigation.php
+++ sharedcode/navigation.php
@@ -6,12 +6,12 @@
 <ul>
 	<li><a href="<?php $root ?>/" title="Need Assistance">Need Assistance</a>
 		<ul>
-			<li><a href="<?php $root ?>/" title="VITA questionnaire">Can VITA help you?</a></li>
-			<li><a href="<?php $root ?>/" title="Signup for an Appointment">Signup for an Appointment</a></li>
+			<li><a href="<?php $root ?>/questionnaire" title="VITA questionnaire">Can VITA help you?</a></li>
+			<li><a href="<?php $root ?>/signup" title="Signup for an Appointment">Signup for an Appointment</a></li>
 		</ul>
 	</li>
 
-	<li><a href="<?php $root ?>/" title="Volunteer">Volunteer</a>
+	<li><a href="<?php $root ?>/volunteer" title="Volunteer">Volunteer</a>
 		<ul>
 
 			<?php if ($USER->isLoggedIn()): ?>
