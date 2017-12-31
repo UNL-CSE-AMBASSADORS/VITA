0c0e2a88c7071182327c3ddfb2482cad86e7c01d
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2017-12-31 11:06:34 -0600

    Archive and make new from template management/users/index.php

--- management/users/index.php
+++ management/users/index.php
@@ -1,85 +1,118 @@
 <?php
-
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+function wdnInclude($path)
+{
+	$documentRoot = 'https://unlcms.unl.edu';
 
-require_once "$root/server/user.class.php";
-$USER = new User();
-if (!$USER->hasPermission('edit_user_permissions')) {
-	header("Location: /unauthorized");
-	die();
+	return readfile($documentRoot . $path);
 }
-
 ?>
-
 <!DOCTYPE html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
 <head>
-	<title>User Management</title>
-	<?php require_once "$root/server/header.php" ?>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css" />
-	<link rel="stylesheet" href="/dist/management/users/users.css" />
+<?php wdnInclude("/wdn/templates_4.1/includes/metanfavico.html"); ?>
+<!--
+	Membership and regular participation in the UNL Web Developer Network is required to use the UNLedu Web Framework. Visit the WDN site at http://wdn.unl.edu/. Register for our mailing list and add your site or server to UNLwebaudit.
+	All framework code is the property of the UNL Web Developer Network. The code seen in a source code view is not, and may not be used as, a template. You may not use this code, a reverse-engineered version of this code, or its associated visual presentation in whole or in part to create a derivative work.
+	This message may not be removed from any pages based on the UNLedu Web Framework.
+
+	$Id: php.fixed.dwt.php | 6edb0e1ee94038935f3821c6ce15dfd5c217b2e2 | Tue Dec 1 17:08:56 2015 -0600 | Kevin Abel  $
+-->
+<?php wdnInclude("/wdn/templates_4.1/includes/scriptsandstyles.html"); ?>
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>VITA Lincoln | University of Nebraska&ndash;Lincoln</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<!-- TemplateParam name="class" type="text" value="" -->
 </head>
-<body>
-	<?php require_once "$root/components/nav.php"; ?>
-	<div class="container">
-		<div class="row pt-5">
-			<div class="col-md-9">
-				<h1>User Management</h1>
-			</div>
-			<div class="col-md-3">
-				<button class="btn btn-default pull-right" id="btn-add-user">Add User</button>
-			</div>
-		</div>
-		<div class="row" id="user-management-table-row">
-			<div class="col-md-12">
-				<table class="table table-condensed table-hover" id="user-management-table">
-					<!-- table data injected through JS -->
-				</table>
-			</div>
-		</div>
-	</div>
-	<div class="hide">
-		<div class="modal" id="add-user-modal" tabindex="-1" role="dialog" aria-hidden="true">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title">Add a new user</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<form id="add-user-form">
-						<div class="modal-body">
-							<section class="form-group">
-								<label for="firstName">First Name:</label>
-								<input type="text" class="form-control" id="firstName">
-							</section>
-							<section class="form-group">
-								<label for="lastName">Last Name:</label>
-								<input type="text" class="form-control" id="lastName">
-							</section>
-							<section class="form-group">
-								<label for="email">Email Address:</label>
-								<input type="text" class="form-control" id="email" placeholder="student@huskers.unl.edu">
-							</section>
-							<section class="form-group">
-								<label for="phone">Phone Number:</label>
-								<input type="text" class="form-control" id="phone">
-							</section>
-						</div>
-						<div class="modal-footer">
-							<button type="submit" class="btn btn-primary">Add User</button>
-							<button type="button" class="btn btn-secondary" data-dismiss="modal" data-target="#add-user-modal">Close</button>
-						</div>
-					</form>
+<body class="@@(_document['class'])@@" data-version="4.1">
+	<?php wdnInclude("/wdn/templates_4.1/includes/skipnav.html"); ?>
+	<div id="wdn_wrapper">
+		<input type="checkbox" id="wdn_menu_toggle" value="Show navigation menu" class="wdn-content-slide wdn-input-driver" />
+		<?php wdnInclude("/wdn/templates_4.1/includes/noscript-padding.html"); ?>
+		<header id="header" role="banner" class="wdn-content-slide wdn-band">
+			<div id="wdn_header_top">
+				<span id="wdn_institution_title"><a href="http://www.unl.edu/">University of Nebraska&ndash;Lincoln</a></span>
+				<div id="wdn_resources">
+					<?php wdnInclude("/wdn/templates_4.1/includes/wdnResources.html"); ?>
+					<?php wdnInclude("/wdn/templates_4.1/includes/idm.html"); ?>
+					<?php wdnInclude("/wdn/templates_4.1/includes/search.html"); ?>
 				</div>
 			</div>
+			<div id="wdn_logo_lockup">
+				<div class="wdn-inner-wrapper">
+					<?php wdnInclude("/wdn/templates_4.1/includes/logo.html"); ?>
+						<span id="wdn_site_affiliation"><!-- TemplateBeginEditable name="affiliation" --><!-- TemplateEndEditable --></span>
+						<span id="wdn_site_title"><!-- TemplateBeginEditable name="titlegraphic" -->VITA Lincoln<!-- TemplateEndEditable --></span>
+				</div>
+			</div>
+		</header>
+		<div id="wdn_navigation_bar" class="wdn-band">
+			<nav id="breadcrumbs" class="wdn-inner-wrapper" role="navigation" aria-label="breadcrumbs">
+				<!-- TemplateBeginEditable name="breadcrumbs" -->
+				<ul>
+					<li><a href="http://www.unl.edu/" title="University of Nebraska&ndash;Lincoln" class="wdn-icon-home">UNL</a></li>
+					<li><a href="/" title="VITA Lincoln">VITA Lincoln</a></li>
+					<li>Home</li>
+				</ul>
+				<!-- TemplateEndEditable -->
+			</nav>
+			<div id="wdn_navigation_wrapper">
+				<nav id="navigation" role="navigation" aria-label="main navigation">
+					<!-- TemplateBeginEditable name="navlinks" -->
+					<?php include "$root/sharedcode/navigation.php"; ?>
+					<!-- TemplateEndEditable -->
+					<?php wdnInclude("/wdn/templates_4.1/includes/navigation-addons.html"); ?>
+				</nav>
+			</div>
 		</div>
+		<div class="wdn-menu-trigger wdn-content-slide">
+			<label for="wdn_menu_toggle" class="wdn-icon-menu">Menu</label>
+			<?php wdnInclude("/wdn/templates_4.1/includes/share.html"); ?>
+		</div>
+		<main id="wdn_content_wrapper" role="main" class="wdn-content-slide" tabindex="-1">
+			<div id="maincontent" class="wdn-main">
+				<div id="pagetitle">
+					<!-- TemplateBeginEditable name="pagetitle" -->
+					<h1>VITA Lincoln</h1>
+					<!-- TemplateEndEditable -->
+				</div>
+				<!-- TemplateBeginEditable name="maincontentarea" -->
+				<div class="wdn-band">
+					<div class="wdn-inner-wrapper">
+						<p>Impress your audience with awesome content!</p>
+					</div>
+				</div>
+				<!-- TemplateEndEditable -->
+			</div>
+		</main>
+		<footer id="footer" role="contentinfo" class="wdn-content-slide">
+			<div id="wdn_optional_footer" class="wdn-band wdn-footer-optional">
+				<div class="wdn-inner-wrapper">
+					<!-- TemplateBeginEditable name="optionalfooter" -->
+					<!-- TemplateEndEditable -->
+				</div>
+			</div>
+			<div id="wdn_local_footer" class="wdn-band wdn-footer-local">
+				<div class="wdn-inner-wrapper">
+					<!-- TemplateBeginEditable name="contactinfo" -->
+					<?php include "$root/sharedcode/localFooter.html"; ?>
+					<!-- TemplateEndEditable -->
+					<!-- TemplateBeginEditable name="leftcollinks" -->
+					<!-- TemplateEndEditable -->
+				</div>
+			</div>
+			<div id="wdn_global_footer" class="wdn-band wdn-footer-global">
+				<div class="wdn-inner-wrapper">
+					<?php wdnInclude("/wdn/templates_4.1/includes/globalfooter.html"); ?>
+				</div>
+			</div>
+		</footer>
+		<?php wdnInclude("/wdn/templates_4.1/includes/noscript.html"); ?>
 	</div>
-
-	<?php require_once "$root/server/footer.php" ?>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
-	<script src="/dist/management/users/users.js"></script>
+	<?php wdnInclude("/wdn/templates_4.1/includes/body_scripts.html"); ?>
+	<?php require_once "$root/server/global_includes.php"; ?>
+	<!-- <script src="/dist/*.js"></script> -->
 </body>
 </html>
