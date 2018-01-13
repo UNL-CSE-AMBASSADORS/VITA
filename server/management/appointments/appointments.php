f3f2219885a2979c4fedd84d6e64e713ad7f16be
Matthew Meacham <mmeacham6@gmail.com>
2018-01-13 14:04:11 -0600

    Remove client information for those without the view all permission

--- server/management/appointments/appointments.php
+++ server/management/appointments/appointments.php
@@ -21,25 +21,37 @@ if (isset($_REQUEST['action'])) {
 }
 
 function getAppointments($year) {
-	GLOBAL $DB_CONN;
+	GLOBAL $DB_CONN, $USER;
 
 	$response = array();
 	$response['success'] = true;
 
+	$canViewClientInformation = $USER->isLoggedIn() && $USER->hasPermission('view_client_information');
+
 	try {
-		$query = "SELECT appointmentId, language, scheduledTime, title, firstName, lastName, Client.phoneNumber, emailAddress
-			FROM Appointment
+		$query = 'SELECT appointmentId, language, scheduledTime, title, firstName, lastName ';
+		if ($canViewClientInformation) {
+			$query .= ", Client.phoneNumber, emailAddress ";
+		}
+
+		$query .= 'FROM Appointment
 				JOIN AppointmentTime ON Appointment.appointmentTimeId = AppointmentTime.appointmentTimeId
 				JOIN Site ON AppointmentTime.siteId = Site.siteId
 				JOIN Client ON Appointment.clientId = Client.clientId
 			WHERE Appointment.archived = FALSE AND
-				YEAR(scheduledTime) = ?";
+				YEAR(scheduledTime) = ?';
 	
 		$stmt = $DB_CONN->prepare($query);
 		$stmt->execute(array($year));
 		$appointments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-		//TODO: NEED TO REMOVE LAST NAME AND STUFF HERE
+		foreach ($appointments as &$appointment) {
+			// Shorten last name to only the initial if user doesn't have permission to view entire last name
+			if (!$canViewClientInformation) {
+				$appointment['lastName'] = substr($appointment['lastName'], 0, 1).'.'; // concat period since this is a last initial
+			}
+			$appointment['language'] = expandLanguageCode($appointment['language']);
+		}
 
 		$response['appointments'] = $appointments;
 	} catch(Exception $e) {
@@ -81,4 +93,12 @@ function rescheduleAppointment($appointmentId, $scheduledTime, $siteId) {
 	}
 
 	echo json_encode($response);
+}
+
+function expandLanguageCode($languageCode) {
+	if ($languageCode === 'eng') return 'English';
+	if ($languageCode === 'spa') return 'Spanish';
+	if ($languageCode === 'vie') return 'Vietnamese';
+	if ($languageCode === 'ara') return 'Arabic';
+	return 'Unknown';
 }
