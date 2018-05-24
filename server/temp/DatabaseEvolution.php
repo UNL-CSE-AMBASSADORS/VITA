4ad42354f66d02349107157210df330485082289
Matthew Meacham <mmeacham6@gmail.com>
2018-05-23 21:45:57 -0700

    Update the DatabaseEvolution file to also set the cancelled field

--- server/temp/DatabaseEvolution.php
+++ server/temp/DatabaseEvolution.php
@@ -15,7 +15,7 @@ function runEvolution() {
 	GLOBAL $DB_CONN;
 
 	# Grab all the serviced appointments
-	$query = 'SELECT notCompletedDescription, appointmentId
+	$query = 'SELECT servicedAppointmentId, notCompletedDescription, appointmentId
 		FROM ServicedAppointment
 		WHERE notCompletedDescription IS NOT NULL';
 	$stmt = $DB_CONN->prepare($query);
@@ -27,7 +27,24 @@ function runEvolution() {
 	$userId = 1; # The id of Matthew Meacham
 	foreach ($servicedAppointments as $servicedAppointment) {
 		$noteAccessor->addNote($servicedAppointment['appointmentId'], $servicedAppointment['notCompletedDescription'], $userId);
+
+		# If the appointment was cancelled as shown in the notCompletedDescription field (the old method), then
+		# we need to set the cancelled field to true
+		$cancelled = $servicedAppointment['notCompletedDescription'] === 'Cancelled Appointment';
+		if ($cancelled) {
+			setCancelledToTrue($servicedAppointment['servicedAppointmentId']);
+		}
 	}
+}
+
+function setCancelledToTrue($servicedAppointmentId) {
+	GLOBAL $DB_CONN;
+
+	$query = 'UPDATE ServicedAppointment
+		SET cancelled = TRUE
+		WHERE servicedAppointmentId = ?';
+	$stmt = $DB_CONN->prepare($query);
+	$stmt->execute(array($servicedAppointmentId));	
 }
 
 runEvolution();
