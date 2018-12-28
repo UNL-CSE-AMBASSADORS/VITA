993966617e10eefd48e6642543ce5d3fc87b50e5
Matthew Meacham <mmeacham6@gmail.com>
2018-12-27 20:08:04 -0600

    Add code to disable appointment sign up until January 10th

--- signup/signup.php
+++ signup/signup.php
@@ -1,14 +1,18 @@
 <?php
 	date_default_timezone_set('America/Chicago');
 	$today = date('Y-m-d');
+	$dateAppointmentSignUpsStart = date('Y-01-10');
 	$taxDay = date('Y-04-15');
+	
 	$taxYear = ($today > $taxDay) ? date('Y', strtotime('+1 year')) : date('Y');
 ?>
 
 <!-- Appointment Signup with no success -->
 <div class="wdn-inner-wrapper wdn-inner-padding-no-top" ng-if="successMessage == null">
-	<?php if ($today < $taxDay) { ?>
+	<?php if ($today > $taxDay) { ?>
 		<h4>VITA appointments have ended for the <?php echo date('Y') ?> tax season. Check back during the <?php echo $taxYear ?> tax season to sign up for an appointment.</h4>
+	<?php } else if ($today < $dateAppointmentSignUpsStart) { ?>
+		<h4>VITA appointment sign-ups begin January 10th, check back then to schedule your appointment.</h4>
 	<?php } else { ?>
 		<form class="cmxform" 
 			id="vitaSignupForm" 
