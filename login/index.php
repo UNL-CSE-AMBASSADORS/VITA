fb12ca376c22895f7cd02ee1b14648cf0f001893
Spencer Collins <CollinsSpencer@users.noreply.github.com>
2019-01-16 12:08:27 -0600

    Update Volunteer Login page

--- login/index.php
+++ login/index.php
@@ -2,173 +2,186 @@
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 function wdnInclude($path)
 {
-	$documentRoot = 'https://unlcms.unl.edu';
-
-	return readfile($documentRoot . $path);
+$documentRoot = 'https://unlcms.unl.edu';
+return readfile($documentRoot . $path);
 }
 ?>
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 <head>
-<?php wdnInclude("/wdn/templates_4.1/includes/metanfavico.html"); ?>
-<!--
-	Membership and regular participation in the UNL Web Developer Network is required to use the UNLedu Web Framework. Visit the WDN site at https://wdn.unl.edu/. Register for our mailing list and add your site or server to UNLwebaudit.
-	All framework code is the property of the UNL Web Developer Network. The code seen in a source code view is not, and may not be used as, a template. You may not use this code, a reverse-engineered version of this code, or its associated visual presentation in whole or in part to create a derivative work.
-	This message may not be removed from any pages based on the UNLedu Web Framework.
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/head-1.html"); ?>
+	<!--
+		Membership and regular participation in the UNL Web Developer Network is required to use the UNLedu Web Framework. Visit the WDN site at http://wdn.unl.edu/. Register for our mailing list and add your site or server to UNLwebaudit.
+		All framework code is the property of the UNL Web Developer Network. The code seen in a source code view is not, and may not be used as, a template. You may not use this code, a reverse-engineered version of this code, or its associated visual presentation in whole or in part to create a derivative work.
+		This message may not be removed from any pages based on the UNLedu Web Framework.
 
-	$Id: php.fixed.dwt.php | 6edb0e1ee94038935f3821c6ce15dfd5c217b2e2 | Tue Dec 1 17:08:56 2015 -0600 | Kevin Abel  $
--->
-<?php wdnInclude("/wdn/templates_4.1/includes/scriptsandstyles.html"); ?>
-<!-- TemplateBeginEditable name="doctitle" -->
-<title>Login | VITA Lincoln | University of Nebraska&ndash;Lincoln</title>
-<!-- TemplateEndEditable -->
-<!-- TemplateBeginEditable name="head" -->
-<!-- TemplateEndEditable -->
-<!-- TemplateParam name="class" type="text" value="" -->
+		$Id: php.fixed.dwt.php | cf0a670a0fd8db9e20a169941c55c838d7c2ba10 | Wed Dec 12 16:54:41 2018 -0600 | Eric Rasmussen	$
+	-->
+	<!-- TemplateBeginEditable name="doctitle" -->
+	<title>VITA Lincoln | University of Nebraska&ndash;Lincoln</title>
+	<!-- TemplateEndEditable -->
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/head-2.html"); ?>
+	<!-- TemplateBeginEditable name="head" -->
+	<!-- Place optional header elements here -->
+	<!-- TemplateEndEditable -->
+	<!-- TemplateParam name="class" type="text" value="" -->
 </head>
-<body class="hide-wdn_identity_management" data-version="4.1">
-	<?php wdnInclude("/wdn/templates_4.1/includes/skipnav.html"); ?>
-	<div id="wdn_wrapper">
-		<input type="checkbox" id="wdn_menu_toggle" value="Show navigation menu" class="wdn-content-slide wdn-input-driver" />
-		<?php wdnInclude("/wdn/templates_4.1/includes/noscript-padding.html"); ?>
-		<header id="header" role="banner" class="wdn-content-slide wdn-band">
-			<div id="wdn_header_top">
-				<span id="wdn_institution_title"><a href="https://www.unl.edu/">University of Nebraska&ndash;Lincoln</a></span>
-				<div id="wdn_resources">
-					<?php wdnInclude("/wdn/templates_4.1/includes/wdnResources.html"); ?>
-					<?php wdnInclude("/wdn/templates_4.1/includes/idm.html"); ?>
-					<?php wdnInclude("/wdn/templates_4.1/includes/search.html"); ?>
-				</div>
-			</div>
-			<div id="wdn_logo_lockup">
-				<div class="wdn-inner-wrapper">
-					<?php wdnInclude("/wdn/templates_4.1/includes/logo.html"); ?>
-						<span id="wdn_site_affiliation"><!-- TemplateBeginEditable name="affiliation" --><!-- TemplateEndEditable --></span>
-						<span id="wdn_site_title"><!-- TemplateBeginEditable name="titlegraphic" -->VITA Lincoln<!-- TemplateEndEditable --></span>
-				</div>
-			</div>
-		</header>
-		<div id="wdn_navigation_bar" class="wdn-band">
-			<nav id="breadcrumbs" class="wdn-inner-wrapper" role="navigation" aria-label="breadcrumbs">
-				<!-- TemplateBeginEditable name="breadcrumbs" -->
-				<ul>
-					<li><a href="https://www.unl.edu/" title="University of Nebraska&ndash;Lincoln" class="wdn-icon-home">UNL</a></li>
-					<li><a href="/" title="VITA Lincoln">VITA Lincoln</a></li>
-					<li><a href="/volunteer" title="Volunteer">Volunteer</a></li>
-					<li>Login</li>
-				</ul>
+<body class="@@(_document['class'])@@ unl" data-version="5.0">
+<?php wdnInclude("/wdn/templates_5.0/includes/global/skip-nav.html"); ?>
+<header class="dcf-header" id="dcf-header" role="banner">
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/header-global-1.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-global-1.html"); ?>
+				<?php wdnInclude("/wdn/templates_5.0/includes/global/visit-global-1.html"); ?>
+				<!-- TemplateBeginEditable name="visitlocal" -->
+				<?php wdnInclude("/wdn/templates_5.0/includes/local/visit-local.html"); ?>
 				<!-- TemplateEndEditable -->
-			</nav>
-			<div id="wdn_navigation_wrapper">
-				<nav id="navigation" role="navigation" aria-label="main navigation">
-					<!-- TemplateBeginEditable name="navlinks" -->
-					<?php include "$root/sharedcode/navigation.php"; ?>
+				<?php wdnInclude("/wdn/templates_5.0/includes/global/visit-global-2.html"); ?>
+				<?php wdnInclude("/wdn/templates_5.0/includes/global/apply-global-1.html"); ?>
+				<!-- TemplateBeginEditable name="applylocal" -->
+				<?php wdnInclude("/wdn/templates_5.0/includes/local/apply-local.html"); ?>
+				<!-- TemplateEndEditable -->
+				<?php wdnInclude("/wdn/templates_5.0/includes/global/apply-global-2.html"); ?>
+				<?php wdnInclude("/wdn/templates_5.0/includes/global/give-global-1.html"); ?>
+				<!-- TemplateBeginEditable name="givelocal" -->
+				<?php wdnInclude("/wdn/templates_5.0/includes/local/give-local.html"); ?>
+				<!-- TemplateEndEditable -->
+				<?php wdnInclude("/wdn/templates_5.0/includes/global/give-global-2.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-global-2.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/idm.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/search.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/header-global-2.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/logo-lockup-1.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/site-affiliation-1.html"); ?>
+	<!-- TemplateBeginEditable name="affiliation" -->
+	<!-- TemplateEndEditable -->
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/site-affiliation-2.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/site-title-1.html"); ?>
+	<!-- TemplateBeginEditable name="titlegraphic" -->
+	<a class="unl-site-title-medium" href="/">VITA Lincoln</a>
+	<!-- TemplateEndEditable -->
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/site-title-2.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/logo-lockup-2.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-toggle-group.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-menu-1.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-toggle-btn.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-menu-child-1.html"); ?>
+	<!-- TemplateBeginEditable name="navlinks" -->
+		<?php include "$root/sharedcode/navigation.php"; ?>
+	<!-- TemplateEndEditable -->
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-menu-child-2.html"); ?>
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/nav-menu-2.html"); ?>
+</header>
+
+<main class="dcf-main" id="dcf-main" role="main" tabindex="-1">
+
+	<!-- TemplateBeginEditable name="hero" -->
+	<div class="dcf-hero dcf-hero-default">
+		<!-- TemplateEndEditable -->
+		<div class="dcf-hero-group-1">
+			<div class="dcf-breadcrumbs-wrapper">
+				<nav class="dcf-breadcrumbs" id="dcf-breadcrumbs" role="navigation" aria-label="breadcrumbs">
+					<!-- TemplateBeginEditable name="breadcrumbs" -->
+					<ol>
+						<li><a href="https://www.unl.edu/">Nebraska</a></li>
+						<li><a href="/">VITA Lincoln</a></li>
+						<li><a href="/volunteer">Volunteer</a></li>
+						<li><span aria-current="page">Login</span></li>
+					</ol>
 					<!-- TemplateEndEditable -->
-					<?php wdnInclude("/wdn/templates_4.1/includes/navigation-addons.html"); ?>
 				</nav>
 			</div>
-		</div>
-		<div class="wdn-menu-trigger wdn-content-slide">
-			<label for="wdn_menu_toggle" class="wdn-icon-menu">Menu</label>
-			<?php wdnInclude("/wdn/templates_4.1/includes/share.html"); ?>
-		</div>
-		<main id="wdn_content_wrapper" role="main" class="wdn-content-slide" tabindex="-1">
-			<div id="maincontent" class="wdn-main">
-				<div id="pagetitle">
-					<!-- TemplateBeginEditable name="pagetitle" -->
-					<h1>Volunteer Login</h1>
-					<!-- TemplateEndEditable -->
-				</div>
-				<!-- TemplateBeginEditable name="maincontentarea" -->
-				<div class="wdn-band">
-					<div id="login_panel" class="wdn-grid-set wdn-inner-wrapper wdn-inner-padding-no-top">
-						<div class="bp768-wdn-col-one-half wdn-pull-right">
-							<form id="login_form">
-								<fieldset>
-									<legend>User Login</legend>
-									<section class="form-group">
-										<label for="login_email">E-mail Address</label>
-										<input id="login_email" class="form-control" placeholder="E-mail" type="text" autocomplete="off" />
-									</section>
-									<section class="form-group">
-										<label for="login_password">Password</label>
-										<input id="login_password" class="form-control" placeholder="Password" type="password" />
-									</section>
-								</fieldset>
-								<input type="submit" value="Login" />
-							</form>
-						</div>
-						<div class="bp768-wdn-col-one-half">
-							<p>
-								<a href="/volunteer">Information about volunteering</a>
-							</p>
-							<p>
-								<a id="login_register_link" href="javascript:void(0);" class="toggle_form" tabindex="-1">
-									Forgot Password/Register
-								</a>
-							</p>
-						</div>
-					</div>
-					<div id="register_panel" class="wdn-grid-set wdn-inner-wrapper wdn-inner-padding-no-top" style="display:none;">
-						<div class="bp768-wdn-col-one-half wdn-pull-right">
-							<form id="register_form" accept-charset="UTF-8" role="form">
-								<fieldset>
-									<legend>Reset Password/Register</legend>
-									<p id="register_info">
-										Please provide the e-mail address associated with your account. Once the form is submitted, you will receive an e-mail with further instructions.
-									</p>
-									<div class="form-group">
-										<label for="register_email">E-mail Address</label>
-										<input id="register_email" class="form-control" placeholder="E-mail" type="text" autocomplete="off" />
-									</div>
-								</fieldset>
-								<input id="register_submit" type="submit" value="Reset Password/Register" />
-							</form>
-							<p id="register_success" style="display:none;">
-								<span style="font-weight:bold; color:#288700;">
-									Your request was received successfully. Please check your email for further instructions.
-								</span>
-							</p>
-						</div>
-						<div class="bp768-wdn-col-one-half">
-							<p>
-								<a id="register_back_link" href="javascript:void(0);" class="toggle_form">
-									<span class="wdn-icon-angle-double-left" aria-hidden="true"></span><span class="wdn-text-hidden">double left arrow icon</span>
-									Back To Login Form
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
+			<header class="dcf-page-title" id="dcf-page-title">
+				<!-- TemplateBeginEditable name="pagetitle" -->
+				<h1>Volunteer Login</h1>
 				<!-- TemplateEndEditable -->
-			</div>
-		</main>
-		<footer id="footer" role="contentinfo" class="wdn-content-slide">
-			<div id="wdn_optional_footer" class="wdn-band wdn-footer-optional">
-				<div class="wdn-inner-wrapper">
-					<!-- TemplateBeginEditable name="optionalfooter" -->
-					<!-- TemplateEndEditable -->
-				</div>
-			</div>
-			<div id="wdn_local_footer" class="wdn-band wdn-footer-local">
-				<div class="wdn-inner-wrapper">
-					<!-- TemplateBeginEditable name="contactinfo" -->
-					<?php include "$root/sharedcode/localFooter.html"; ?>
-					<!-- TemplateEndEditable -->
-					<!-- TemplateBeginEditable name="leftcollinks" -->
-					<!-- TemplateEndEditable -->
-				</div>
-			</div>
-			<div id="wdn_global_footer" class="wdn-band wdn-footer-global">
-				<div class="wdn-inner-wrapper">
-					<?php wdnInclude("/wdn/templates_4.1/includes/globalfooter.html"); ?>
-				</div>
-			</div>
-		</footer>
-		<?php wdnInclude("/wdn/templates_4.1/includes/noscript.html"); ?>
+			</header>
+			<!-- TemplateBeginEditable name="herogroup1" -->
+			<!-- TemplateEndEditable -->
+		</div>
+		<!-- TemplateBeginEditable name="herogroup2" -->
+		<div class="dcf-hero-group-2">
+		</div>
+		<!-- TemplateEndEditable -->
 	</div>
-	<?php wdnInclude("/wdn/templates_4.1/includes/body_scripts.html"); ?>
-	<?php require_once "$root/server/global_includes.php"; ?>
-	<script src="/dist/login/login.js"></script>
+	<div class="dcf-main-content dcf-wrapper">
+		<!-- TemplateBeginEditable name="maincontentarea" -->
+		<div class="dcf-bleed">
+			<div id="login_panel" class="dcf-grid dcf-wrapper dcf-pb-10">
+				<div class="dcf-col-100% dcf-1st dcf-col-50%-end@md dcf-2nd@md dcf-pb-5">
+					<form id="login_form">
+						<fieldset>
+							<legend>User Login</legend>
+							<section class="form-group">
+								<label for="login_email">E-mail Address</label>
+								<input id="login_email" class="form-control" placeholder="E-mail" type="text" autocomplete="off" />
+							</section>
+							<section class="form-group">
+								<label for="login_password">Password</label>
+								<input id="login_password" class="form-control" placeholder="Password" type="password" />
+							</section>
+						</fieldset>
+						<input class="dcf-mt-5" type="submit" value="Login" />
+					</form>
+				</div>
+				<div class="dcf-col-100% dcf-2nd dcf-col-50%-start@md dcf-1st@md">
+					<p>
+						<a href="/volunteer">Information about volunteering</a>
+					</p>
+					<p>
+						<a id="login_register_link" href="javascript:void(0);" class="toggle_form" tabindex="-1">
+							Forgot Password/Register
+						</a>
+					</p>
+				</div>
+			</div>
+			<div id="register_panel" class="dcf-grid dcf-wrapper dcf-pb-10" style="display:none;">
+				<div class="dcf-col-100% dcf-1st dcf-col-50%-end@md dcf-2nd@md dcf-pb-5">
+					<form id="register_form" accept-charset="UTF-8" role="form">
+						<fieldset>
+							<legend>Reset Password/Register</legend>
+							<p id="register_info">
+								Please provide the e-mail address associated with your account. Once the form is submitted, you will receive an e-mail with further instructions.
+							</p>
+							<div class="form-group">
+								<label for="register_email">E-mail Address</label>
+								<input id="register_email" class="form-control" placeholder="E-mail" type="text" autocomplete="off" />
+							</div>
+						</fieldset>
+						<input class="dcf-mt-5" id="register_submit" type="submit" value="Reset Password/Register" />
+					</form>
+					<p id="register_success" style="display:none;">
+						<span style="font-weight:bold; color:#288700;">
+							Your request was received successfully. Please check your email for further instructions.
+						</span>
+					</p>
+				</div>
+				<div class="dcf-col-100% dcf-2nd dcf-col-50%-start@md dcf-1st@md">
+					<p>
+						<a id="register_back_link" href="javascript:void(0);" class="toggle_form">
+							<span class="wdn-icon-angle-double-left" aria-hidden="true"></span><span class="wdn-text-hidden">double left arrow icon</span>
+							Back To Login Form
+						</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<!-- TemplateEndEditable -->
+	</div>
+</main>
+<footer class="dcf-footer" id="dcf-footer" role="contentinfo">
+	<!-- TemplateBeginEditable name="optionalfooter" -->
+	<!-- TemplateEndEditable -->
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/footer-global-1.html"); ?>
+	<!-- TemplateBeginEditable name="contactinfo" -->
+		<?php include "$root/sharedcode/localFooter.html"; ?>
+	<!-- TemplateEndEditable -->
+		<?php wdnInclude("/wdn/templates_5.0/includes/global/footer-global-2.html"); ?>
+</footer>
+<?php wdnInclude("/wdn/templates_5.0/includes/global/noscript.html"); ?>
+<?php wdnInclude("/wdn/templates_5.0/includes/global/js-body.html"); ?>
+<!-- TemplateBeginEditable name="jsbody" -->
+<?php require_once "$root/server/global_includes.php"; ?>
+<script src="/dist/login/login.js"></script>
+<!-- TemplateEndEditable -->
 </body>
 </html>
