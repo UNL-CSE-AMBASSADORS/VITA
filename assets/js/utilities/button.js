bbb23ce8b1c77f8879e06444727bd182ee879534
Matthew Meacham <mmeacham6@gmail.com>
2019-08-10 21:27:11 -0500

    Use const over var in button.js

--- assets/js/utilities/button.js
+++ assets/js/utilities/button.js
@@ -5,12 +5,12 @@ define('toggleDirective', [], function() {
 		function link(scope, element, attrs) {
 
 			if (attrs.toggle == "buttons") {
-				var activeClassName = 'dcf-btn-primary';
-				var nonActiveClassName = 'dcf-btn-secondary';
-				
+				const activeClassName = 'dcf-btn-primary';
+				const nonActiveClassName = 'dcf-btn-secondary';
+
 				// Note, this makes all data-toggle="buttons" behave like radio buttons
 				element.find('label').on('click', function (e) {
-					var $button = angular.element(e.target);
+					const $button = angular.element(e.target);
 					element.children().removeClass(`active ${activeClassName}`).addClass(`${nonActiveClassName}`);
 					$button.addClass(`active ${activeClassName}`).removeClass(`${nonActiveClassName}`);
 				});
