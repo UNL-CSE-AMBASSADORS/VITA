dbf1421b988f1b8573a003e8ce47faf269f2b146
jcarrigan2 <42622108+jcarrigan2@users.noreply.github.com>
2021-02-23 17:55:45 -0600

    Commit

--- components/appointmentPicker/appointmentPickerDataService.js
+++ components/appointmentPicker/appointmentPickerDataService.js
@@ -3,8 +3,9 @@ define('appointmentPickerDataService', [], function($http) {
 	function appointmentPickerDataService($http) {
 		return {
 			loadAllAppointments: function(year, appointmentType = "residential") {
+				// Is this able to call anything? I only see getAppointmentTimes in getTimes.php
 				return $http.get(`/server/api/appointments/getTimes.php?action=getAppointments&year=${year}&appointmentType=${appointmentType}`).then(function(response){
-					return response.data;
+					return response.data; 
 				},function(error){
 					return null;
 				});
